357fa0b6127ca5371376d9bb0c5d07978b371e97
Ray Marceau <razor2n4@gmail.com>
2014-08-17 17:35:02 -0700

    oops... I can't actually delete the module controllers file!

--- init2.js
+++ init2.js
@@ -33,6 +33,7 @@ require('./app/directives/hylo_post');
 require('./app/directives/embeddedComments');
 require('./app/features/billing/billing');
 require('./app/features/seeds/seeds');
+require('./app/controllers');
 require('./app/controllers/community_users');
 require('./app/controllers/community');
 require('./app/controllers/menu');
