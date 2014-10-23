a94746651176e6f9266724de3bb1363e62fe0dd9
Lawrence Wang <l@lw.io>
2014-10-23 12:25:51 -0700

    tweak karma config
    setting singleRun: false in the config and then using the —single-run
    arg seems to confuse things, so let’s do it the other way around
    
    Conflicts:
    	package.json

--- karma.config.js
+++ karma.config.js
@@ -49,17 +49,17 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_WARN,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
