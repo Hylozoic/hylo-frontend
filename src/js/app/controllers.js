6a0eb1662a2c10ea251808aa9f8e113a7121bf70
Lawrence Wang <l@lw.io>
2014-10-23 16:38:34 -0700

    move controller requires into controllers.js

--- src/js/app/controllers.js
+++ src/js/app/controllers.js
@@ -1,1 +1,11 @@
 angular.module('hyloControllers', ['hyloFilters']);
+
+require('./controllers/comments');
+require('./controllers/community_users');
+require('./controllers/community');
+require('./controllers/fulfillModal');
+require('./controllers/onboarding');
+require('./controllers/profile_settings');
+require('./controllers/search');
+require('./controllers/user');
+require('./controllers/view_post');
