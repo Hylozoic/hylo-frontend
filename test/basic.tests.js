8e2323eae4b531e09e42f06aca6468ed22b2c8a4
Lawrence Wang <l@lw.io>
2014-10-22 19:16:12 -0700

    set up karma for automated js testing
    Conflicts:
    	package.json

---
+++ test/basic.tests.js
@@ -0,0 +1,11 @@
+describe('hylo-frontend', function() {
+
+  it('does basic math correctly', function() {
+    expect(4 + 4).toEqual(8);
+  })
+
+  it('loads angular', function() {
+    expect(typeof(angular)).toEqual('object');
+  });
+
+});
