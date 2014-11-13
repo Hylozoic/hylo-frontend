5a9b3a490264286ff4116883d672531204215179
Lawrence Wang <l@lw.io>
2014-11-12 16:27:38 -0800

    wire up invite form
    double-clicking is prevented
    error messages are not pretty, but who cares

--- src/js/app/controllers/community_settings.js
+++ src/js/app/controllers/community_settings.js
@@ -1,15 +1,29 @@
 module.exports = function(angularModule) {
 
   angularModule.controller("CommunitySettingsCtrl", [
-    "$scope", '$timeout', '$state', '$log', '$analytics',
-    function ($scope, $timeout, $state, $log, $analytics) {
+    "$scope", '$timeout', '$state', '$log', '$analytics', 'Community',
+    function ($scope, $timeout, $state, $log, $analytics, Community) {
 
       $scope.close = function() {
         $state.go('community', {community: $scope.community.slug});
       };
 
       $scope.submit = function() {
-        // TODO
+        if ($scope.submitting) return;
+        $scope.submitting = true;
+
+        Community.invite({id: $scope.community.id, emails: $scope.emails})
+        .$promise.then(function(resp) {
+          if (resp.sent > 1)
+            alert('Your invitations are on their way!');
+          else
+            alert('Your invitation is on its way!');
+          $scope.emails = '';
+          $scope.submitting = false;
+        }, function() {
+          alert('Something went wrong. Please check the emails you entered for typos.');
+          $scope.submitting = false;
+        });
       };
 
     }
