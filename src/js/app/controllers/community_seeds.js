f376cd2974be60cb54003dea8ed46a9f35ca6f5c
Minda Myers <minda@hylo.com>
2014-11-14 22:05:04 -0800

    added less and controller files for tab navigation

---
+++ src/js/app/controllers/community_seeds.js
@@ -0,0 +1,6 @@
+angular.module("hyloControllers").controller('CommunitySeedsCtrl', ['$scope', '$rootScope', 'Post', 'growl', '$timeout', '$http', '$q', '$modal', '$analytics', '$state',
+  function($scope, $rootScope, Post, growl, $timeout, $http, $q, $modal, $analytics, $state) {
+
+  	console.log("set up community seeds controller");
+
+  }]);
