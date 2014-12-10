f2321eb308f19c130e2b1e8b1d98cf48ef4a00a9
Lawrence Wang <l@lw.io>
2014-12-09 20:05:54 -0800

    factor filepickerUpload into a service
    but require it (thanks browserify!) instead of using angular’s
    dependency injection. hmm. i wonder which one of those will win out in
    the long term.

--- src/js/app/controllers/community_settings.js
+++ src/js/app/controllers/community_settings.js
@@ -1,29 +1,4 @@
-var filepickerUpload = function(opts) {
-  var pickOptions = {
-    mimetype: 'image/*',
-    multiple: false,
-    services: ['COMPUTER', 'FACEBOOK', 'WEBCAM', 'GOOGLE_DRIVE', 'DROPBOX', 'INSTAGRAM', 'FLICKR', 'URL'],
-    folders: false
-  };
-
-  var storeOptions = {
-    location: 'S3',
-    container: hyloEnv.s3.bucket,
-    path: 'orig/',
-    access: 'public'
-  };
-
-  var convert = function(blobs) {
-    var blob = blobs[0],
-      convertStoreOptions = $.extend(storeOptions, {path: opts.path + blob.key.substring(5)});
-
-    filepicker.convert(blob, opts.convert, convertStoreOptions, function(newBlob) {
-      opts.success(hyloEnv.s3.cloudfrontHost + '/' + newBlob.key);
-    }, opts.failure)
-  };
-
-  filepicker.pickAndStore(pickOptions, storeOptions, convert, opts.failure);
-};
+var filepickerUpload = require('../services/filepickerUpload');
 
 module.exports = function(angularModule) {
 
