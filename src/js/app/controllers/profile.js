56dc911e1c9ebe406a0c95f53e0ea70276dd3bb6
Minda Myers <minda@hylo.com>
2014-12-11 20:31:57 -0800

    css styles for the profile banner

--- src/js/app/controllers/profile.js
+++ src/js/app/controllers/profile.js
@@ -1,5 +1,23 @@
-var dependencies = ['$scope'];
-dependencies.push(function($scope) {
+var dependencies = ['$scope', '$analytics', '$stateParams', 'OldUser'];
+dependencies.push(function($scope, $analytics, $stateParams, OldUser) {
+	$scope.hello = 'on two lines';
+
+  $scope.currentUser.$promise.then(function(user) {
+    var isOwnProfile = (user.id == $stateParams.id);
+    $analytics.eventTrack('User: Load a Member Profile', {user_id: user.id, is_own_profile: isOwnProfile});
+    if (isOwnProfile) {
+      $scope.user = user;
+      $scope.editable = true;
+
+    } else {
+      $scope.user = OldUser.get({id: $stateParams.id});
+      $scope.editable = false;
+    }
+    console.log("user: ");
+    console.dir($scope.user);
+  });
+
+
 });
 
 module.exports = function(angularModule) {
