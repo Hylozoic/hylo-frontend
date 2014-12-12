2bc2f68098073c003dbe634e1abfbc5b29a0f42b
Lawrence Wang <l@lw.io>
2014-12-11 16:41:14 -0800

    factor out trailing slash remover into a service
    and write tests for it

--- src/js/app/hyloApp.js
+++ src/js/app/hyloApp.js
@@ -36,17 +36,7 @@ angular.module('hyloApp', [
     $idleProvider.warningDuration(1); // in seconds
 
     // remove trailing slashes from paths
-    $urlRouterProvider.rule(function ($injector, $location) {
-      var path = $location.url();
-
-      if (path[path.length - 1] === '/') {
-        return path.substring(0, path.length - 1);
-      }
-
-      if (path.indexOf('/?') > -1) {
-        return path.replace('/?', '?');
-      }
-    });
+    $urlRouterProvider.rule(require('./services/removeTrailingSlash'));
 
     // Disable bootstrap UI animations
     $tooltipProvider.options({
