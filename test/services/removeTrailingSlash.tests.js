4ba007c44d6ea73f739b18c4191eafce064acb78
Lawrence Wang <l@lw.io>
2014-12-11 16:42:35 -0800

    add missing semicolon

--- test/services/removeTrailingSlash.tests.js
+++ test/services/removeTrailingSlash.tests.js
@@ -12,7 +12,7 @@ describe('removeTrailingSlash', function() {
   it('removes the slash from a simple path', function() {
     var output = removeTrailingSlash(null, location('/foo/bar/'))
     expect(output).toEqual('/foo/bar');
-  })
+  });
 
   it('removes the slash from a path with query parameters', function() {
     var output = removeTrailingSlash(null, location('/foo/bar/?baz=bonk'));
