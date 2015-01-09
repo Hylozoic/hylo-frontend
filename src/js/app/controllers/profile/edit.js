70e0ebc6909566510c8f91a3357329a5bc41a572
Lawrence Wang <l@lw.io>
2015-01-08 17:08:32 -0800

    editing skills and affiliations

--- src/js/app/controllers/profile/edit.js
+++ src/js/app/controllers/profile/edit.js
@@ -2,7 +2,7 @@ var dependencies = ['$scope', '$analytics', 'newCurrentUser'];
 dependencies.push(function($scope, $analytics, newCurrentUser) {
   var user = $scope.user = newCurrentUser;
 
-  $scope.editData = _.pick(user, ['bio']);
+  $scope.editData = _.pick(user, ['bio', 'skills', 'organizations']);
 
   $scope.save = function() {
     user.update($scope.editData, function() {
@@ -13,6 +13,14 @@ dependencies.push(function($scope, $analytics, newCurrentUser) {
 
   $scope.cancel = function() {
     $scope.$state.go('profile.seeds', {id: user.id});
+  };
+
+  $scope.add = function(event, type) {
+    if (event.which == 13) {
+      $scope.editData[type].unshift(event.target.value);
+      event.target.value = '';
+    }
+    return true;
   };
 });
 
