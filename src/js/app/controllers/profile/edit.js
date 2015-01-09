eefa074b05015133551c7061b7a52142bdbde34d
Lawrence Wang <l@lw.io>
2015-01-08 17:41:21 -0800

    don't update skills & affliations if not changed

--- src/js/app/controllers/profile/edit.js
+++ src/js/app/controllers/profile/edit.js
@@ -1,9 +1,13 @@
 var dependencies = ['$scope', '$analytics', 'newCurrentUser'];
 dependencies.push(function($scope, $analytics, newCurrentUser) {
-  var user = $scope.user = newCurrentUser;
-  var editData = $scope.editData = _.pick(user, ['bio', 'skills', 'organizations']);
+  var user = $scope.user = newCurrentUser,
+    editData = $scope.editData = _.pick(user, ['bio', 'skills', 'organizations']),
+    edited = {};
 
   $scope.save = function() {
+    if (!edited.skills) delete editData.skills;
+    if (!edited.organizations) delete editData.organizations;
+
     user.update(editData, function() {
       _.extend(user, editData);
       $scope.cancel();
@@ -18,12 +22,14 @@ dependencies.push(function($scope, $analytics, newCurrentUser) {
     if (event.which == 13) {
       editData[type].unshift(event.target.value);
       event.target.value = '';
+      edited[type] = true;
     }
     return true;
   };
 
   $scope.remove = function(value, type) {
     editData[type].splice(editData[type].indexOf(value), 1);
+    edited[type] = true;
   }
 });
 
