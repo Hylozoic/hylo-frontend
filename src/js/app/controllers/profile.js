e22d6f10f988957d9101862face8920126ee1e78
Lawrence Wang <l@lw.io>
2015-01-12 15:05:39 -0800

    wire up frontend for counts

--- src/js/app/controllers/profile.js
+++ src/js/app/controllers/profile.js
@@ -1,11 +1,24 @@
-var dependencies = ['$scope', '$analytics', 'User', 'user', 'editable'];
-dependencies.push(function($scope, $analytics, User, user, editable) {
+var dependencies = ['$scope', '$analytics', 'User', 'user', 'editable', 'Post'];
+dependencies.push(function($scope, $analytics, User, user, editable, Post) {
   $scope.user = user;
   $scope.editable = editable;
 
   if (!user.banner_url) {
     user.banner_url = require('../services/defaultUserBanner');
   }
+
+  $scope.hasPosts = true;
+
+  Post.forUser({userId: user.id}).$promise.then(function(posts) {
+    $scope.posts = posts;
+    $scope.hasPosts = posts.length > 0;
+  });
+
+  $scope.removePost = function(post) {
+    growl.addSuccessMessage("Seed has been removed: " + post.name, {ttl: 5000});
+    $scope.posts.splice($scope.posts.indexOf(post), 1);
+  };
+
 });
 
 module.exports = function(angularModule) {
