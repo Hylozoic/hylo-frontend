f2d3e8633d5bc884e5844cf9ffd663a072c82acf
Minda Myers <minda@hylo.com>
2015-01-15 16:06:16 -0800

    implemented button actions on user settings

--- src/js/app/controllers/user_profile.js
+++ src/js/app/controllers/user_profile.js
@@ -1,6 +1,30 @@
-angular.module("hyloControllers").controller('UserSettingsCtrl', ['$rootScope', 'growl', '$analytics', '$modal', '$log', '$state',
-  function($rootScope, growl, $analytics, $modal, $log, $state) {
+angular.module("hyloControllers").controller('UserSettingsCtrl', ['$scope', '$rootScope', 'growl', '$analytics', '$modal', '$log', '$state',
+  function($scope, $rootScope, growl, $analytics, $modal, $log, $state) {
 
    console.log("loaded user settings");
+
+   $scope.close = function() {
+      //todo: direct to member profile page
+      //$state.go('community', {community: $scope.community.slug});
+    };
+
+    $scope.editing = {};
+    $scope.edited = {};
+
+    $scope.edit = function(field) {
+      $scope.edited[field] = $scope.community[field];
+      $scope.editing[field] = true;
+    };
+
+    $scope.cancelEdit = function(field) {
+      $scope.editing[field] = false;
+    };
+
+    $scope.saveEdit = function(field) {
+      $scope.editing[field] = false;
+      var data = {id: $scope.community.id};
+      data[field] = $scope.edited[field];
+      //save data via API
+    };
 
   }]);
