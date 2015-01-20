b8fd716e7a367b82e49d8971cd9f42e0d90d2e1b
Minda Myers <minda@hylo.com>
2015-01-19 22:30:37 -0800

    Seed type selector, update UI based on seed type

--- src/js/app/controllers/NewSeedCtrl.js
+++ src/js/app/controllers/NewSeedCtrl.js
@@ -1,6 +1,25 @@
 var dependencies = ['$scope'];
 dependencies.push(function($scope) {
   $scope.foo = 'foo';
+
+  //$scope.seedType = 'intention';
+
+  $scope.switchSeedType = function(seedType) {
+  	$scope.seedType = seedType;
+  	if (seedType === 'intention') {
+  		$scope.placeholderText = "I'd like to create ";
+  	}
+  	else if (seedType === 'offer') {
+  		$scope.placeholderText = "I'd like to share ";
+
+  	}
+  	else if (seedType === 'request') {
+  		$scope.placeholderText = "I'm looking for ";
+  	}
+  }
+
+  $scope.switchSeedType('intention');
+
 });
 
 module.exports = function(angularModule) {
