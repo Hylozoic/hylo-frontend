5b084b809a5e5ce9263b14f205a150f6eb18a3f0
Minda Myers <minda@hylo.com>
2015-02-20 19:20:12 -0800

    installing intercom in javascript file

--- src/js/onload.js
+++ src/js/onload.js
@@ -7,8 +7,7 @@ hyloEnv.onUser(function(user) {
     lastName: user.last_name,
     name: user.name,
     provider: user.linkedAccounts[0].provider_key,
-    created: user.date_created,
-    createdAt: user.date_created
+    created: user.date_created
   }, {
     integrations: {
       Intercom: {
@@ -23,6 +22,48 @@ hyloEnv.onUser(function(user) {
     username: user.name,
     email: user.email
   };
+
+  //intercom
+  window.intercomSettings = {
+    name: user.name,
+    email: user.email,
+    created_at: user.date_created,
+    app_id: "wwelodje"
+  };
+
+(function(){
+  var w=window;
+  var ic=w.Intercom;
+  if(typeof ic==="function"){
+    ic('reattach_activator');
+    ic('update',intercomSettings);
+  }
+  else {
+    var d=document;
+    var i=function() {
+      i.c(arguments)
+    };
+    i.q=[];
+    i.c=function(args) {
+      i.q.push(args)
+    };
+    w.Intercom=i;
+    function l(){
+      var s=d.createElement('script');
+      s.type='text/javascript';
+      s.async=true;
+      s.src='https://widget.intercom.io/widget/wwelodje';
+      var x=d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s,x);
+    }
+    if(w.attachEvent){
+      w.attachEvent('onload',l);
+    }
+    else {
+      w.addEventListener('load',l,false);
+    }
+  }
+})()
 
 });
 
