1e75a0b581ef12bbb6469ce652ed119aae728d71
Minda Myers <minda@hylo.com>
2015-02-20 19:26:28 -0800

    take out zopim

--- src/js/onload.js
+++ src/js/onload.js
@@ -67,27 +67,6 @@ hyloEnv.onUser(function(user) {
 
 });
 
-// Zopim
-if (hyloEnv.isProd) {
-  var ua = navigator.userAgent.toLowerCase(),
-  platform = navigator.platform.toLowerCase();
-  platformName = ua.match(/ip(?:ad|od|hone)/) ? 'ios' : (ua.match(/(?:webos|android)/) || platform.match(/mac|win|linux/) || ['other'])[0],
-  isMobile = /ios|android|webos/.test(platformName);
-  if (!isMobile) {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?1xg3kCDzmdR6YP4N2pijAkN8yitxHFrJ';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-
-    hyloEnv.onUser(function(user) {
-      $zopim(function() {
-        $zopim.livechat.setName(user.name);
-        $zopim.livechat.setEmail(user.email);
-      });
-    });
-  }
-}
 
 window.fbAsyncInit = function() {
   FB.init({appId: hyloEnv.fb.appId, xfbml: true, version: 'v2.1'});
