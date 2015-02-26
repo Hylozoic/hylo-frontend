67897f9574e83b9ebb03703a7cf0bb57f4b45034
Lawrence Wang <l@lw.io>
2015-02-25 19:14:08 -0800

    refactor auth_module behavior a bit

--- src/js/app/auth_module.js
+++ src/js/app/auth_module.js
@@ -1,28 +1,24 @@
 angular.module('hylo-auth-module', ['http-auth-interceptor'])
-/**
- * This directive will find itself inside HTML as a class,
- * and will remove that class, so CSS will remove loading image and show app content.
- * It is also responsible for showing/hiding login form.
- */
-.directive('hyloAuth', ['$timeout', function($timeout) {
+.directive('hyloAuth', function($timeout) {
   return {
-    restrict: 'C',
+    restrict: 'A',
     link: function($scope, $elem, $attrs) {
       //once Angular is started, remove class:
       $elem.removeClass('waiting-for-angular');
 
-      $scope.loginMode = false;
+      $scope.loggedIn = true;
 
       $scope.$on('event:auth-loginRequired', function() {
         $timeout(function() {
           window.location.replace("/login");
         }, 3000);
 
-        $scope.loginMode = true;
+        $scope.loggedIn = false;
       });
+
       $scope.$on('event:auth-loginConfirmed', function() {
-        $scope.loginMode = false;
+        $scope.loggedIn = true;
       });
     }
   }
-}]);
+});
