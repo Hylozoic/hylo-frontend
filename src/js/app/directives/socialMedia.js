b892356181c1dc80eaa5eb7ee15f2cb6512a8145
Minda Myers <minda@hylo.com>
2015-02-27 18:18:44 -0800

    Revert "port functions from profile controller to the social media directive"
    This reverts commit 6e436dbe9d1e0a658ba261030ac0b51a8d8ee31f.

--- src/js/app/directives/socialMedia.js
+++ src/js/app/directives/socialMedia.js
@@ -1,22 +1,11 @@
-var directive = function($analytics) {
+var directive = function() {
 
   var controller = function($scope, $element) {
-
-    $scope.twitterUrl = function() {
-      return 'https://twitter.com/' + $scope.user.twitter_name;
-    };
-
-    $scope.clickedSocialLink = function(network, url) {
-      $analytics.eventTrack('Clicked a Social Media link', {'network': network, 'url': url, 'page': $scope.page});
-    };
+    console.log("controller works");
   };
 
   return {
     restrict: 'E',
-    scope: {
-      user: '=',
-      page: '='
-    },
     templateUrl: "/ui/app/socialMedia.tpl.html",
     controller: controller
   };
