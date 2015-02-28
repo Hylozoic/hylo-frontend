de832f7cfd4d8ac18215b95b8a329c41c749ecf2
Minda Myers <minda@hylo.com>
2015-02-27 21:21:01 -0800

    fine UI tweaks for member directory
    aligned the lone message icon way to the left,
    aligned everything left,
    consistent spacing,
    made sure members page social media links and mobile views aligned real
    good

--- src/js/app/directives/socialMedia.js
+++ src/js/app/directives/socialMedia.js
@@ -2,11 +2,17 @@ var directive = function($analytics) {
 
   var controller = function($scope, $element) {
 
-    console.dir($scope);
-
     $scope.twitterUrl = function() {
       return 'https://twitter.com/' + $scope.user.twitter_name;
     };
+
+    $scope.hasSocialMediaLink = function() {
+      var found = false;
+      if ($scope.user.twitter_name || $scope.user.linkedin_url || $scope.user.facebook_url)
+        found = true;
+      if (found)  console.log('has link');
+      return found;
+    }
 
     $scope.clickedSocialLink = function(network, url) {
       $analytics.eventTrack('Clicked a Social Media link', {'network': network, 'url': url, 'page': $scope.page});
