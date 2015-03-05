2657ad2cb176af5b5c621cbb0728ca821f8d3dfa
Lawrence Wang <l@lw.io>
2015-03-04 17:45:57 -0800

    fix the limiting of autocomplete results for @-mentions

--- src/js/app/features/mentions/userMentions.js
+++ src/js/app/features/mentions/userMentions.js
@@ -1,23 +1,15 @@
 module.exports = function(angularModule) {
 
-  angularModule.service('UserMentions', ['$q', function($q) {
+  angularModule.service('UserMentions', function(Community) {
     return {
       userTextRaw: function(user) {
         return '<a contenteditable="false" tabindex="-1" target="_blank" href="/u/' + user.id + '" data-user-id="' + user.id + '">@' + user.name + '</a>'
       },
 
       searchPeople: function(query, community) {
-        var peopleList = [];
-        return community.members({search: query, limit: 5}).$promise.then(function (items) {
-          angular.forEach(items, function(item) {
-            if (item.name.toUpperCase().indexOf(query.toUpperCase()) >= 0) {
-              peopleList.push(item);
-            }
-          });
-          return peopleList;
-        });
+        return Community.findMembers({id: community.id, search: query, limit: 5});
       }
 
     };
-  }]);
+  });
 };
