242a2296a97e497394c487a0070771eec9fed9c2
Lawrence Wang <l@lw.io>
2015-03-09 12:49:08 -0700

    fix for onboarding
    the order in which OverlayCtrl and CommunitySeedsCtrl load turns out to
    be unspecified, so i have to have them communicate via a service
    instead of an event in case the former isn’t loaded first

--- src/js/app/services.js
+++ src/js/app/services.js
@@ -1,5 +1,26 @@
 var angularModule = angular.module('hyloServices', ['ngResource']).
 
+factory('Overlay', function($rootScope) {
+  // just need a state object here
+  // in order to account for the fact that sometimes
+  // when we want to set up the overlay, OverlayCtrl
+  // has not yet been initialized
+  var storedData;
+
+  return {
+    show: function(data) {
+      storedData = data;
+      $rootScope.$emit('overlay:load', data);
+    },
+    resetData: function() {
+      storedData = null;
+    },
+    storedData: function() {
+      return storedData;
+    }
+  };
+}).
+
 factory('CurrentUser', ['$resource',
   function($resource) {
     return $resource('/current_user', {});
