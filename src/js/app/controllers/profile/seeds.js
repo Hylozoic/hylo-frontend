5834881eec5f22b1aba54fc393d45406f1f272f5
Lawrence Wang <l@lw.io>
2015-03-09 17:25:04 -0700

    fix missing dependency

--- src/js/app/controllers/profile/seeds.js
+++ src/js/app/controllers/profile/seeds.js
@@ -1,16 +1,15 @@
-var dependencies = ['$scope', '$analytics', 'posts',];
-dependencies.push(function($scope, $analytics, posts ) {
+var controller = function($scope, $analytics, posts, growl) {
 	$scope.posts = posts;
 	$scope.hasPosts = posts.length > 0;
 
 	$scope.removePost = function(post) {
-	    growl.addSuccessMessage("Seed has been removed: " + post.name, {ttl: 5000});
-	    $analytics.eventTrack('Post: Remove a Seed', {post_name: post.name, post_id: post.id});
-	    $scope.posts.splice($scope.posts.indexOf(post), 1);
-	  };
+    growl.addSuccessMessage("Seed has been removed: " + post.name, {ttl: 5000});
+    $analytics.eventTrack('Post: Remove a Seed', {post_name: post.name, post_id: post.id});
+    $scope.posts.splice($scope.posts.indexOf(post), 1);
+  };
 
-});
+};
 
 module.exports = function(angularModule) {
-  angularModule.controller('ProfileSeedsCtrl', dependencies);
+  angularModule.controller('ProfileSeedsCtrl', controller);
 };
