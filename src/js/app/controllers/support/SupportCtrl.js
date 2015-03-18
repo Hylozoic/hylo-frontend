1f7b0f3b756654b541f42733edd6db2b06a47e21
Minda Myers <minda@hylo.com>
2015-03-18 14:14:56 -0700

    basic styling for the help page

--- src/js/app/controllers/support/SupportCtrl.js
+++ src/js/app/controllers/support/SupportCtrl.js
@@ -1,7 +1,18 @@
 
 
-var controller = function($scope, $analytics) {
+var controller = function($scope, $analytics, $history, $rootScope, Community) {
   console.log("help");
+
+  console.dir($scope);
+  console.dir($rootScope)
+
+  $scope.close = function() {
+    if ($history.isEmpty()) {
+      //$state.go('community.seeds', {community: community.slug});
+    } else {
+      $history.go(-1);
+    }
+  };
 };
 
 module.exports = function(angularModule) {
