eaca6ce45614f7d91f89559866dcb44cf55cfa6c
Lawrence Wang <l@lw.io>
2015-04-15 15:04:03 -0700

    don't resume onboarding if on profile step

--- src/js/app/services/onboarding.js
+++ src/js/app/services/onboarding.js
@@ -135,7 +135,7 @@ var factory = function($timeout, $resource, $rootScope, $state, $analytics, Over
       // TODO maybe only do this if within the first few steps?
       // i.e. once we get to community step, we can allow open-ended
       // exploration and only continue onboarding through non-modal prompts
-      if (this.currentStep() === 'done' || $state.$current.name === steps[this.currentStep()].state)
+      if (_.include(['profile', 'done'], this.currentStep()) || $state.$current.name === steps[this.currentStep()].state)
         return;
 
       this._track('Resume', {from: $state.$current.name, to: steps[this.currentStep()].state});
