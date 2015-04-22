a5a89e09f14939f901d243019ba7a83741e06bc3
Lawrence Wang <l@lw.io>
2015-04-22 00:15:35 -0700

    facebook login/signup

--- src/js/app/services/ThirdPartyAuth.js
+++ src/js/app/services/ThirdPartyAuth.js
@@ -17,6 +17,19 @@ var factory = function() {
           'googleAuth',
           format('width=%s, height=%s, titlebar=no, toolbar=no, menubar=no, left=%s, top=%s', width, height, left, top)
         );
+
+      } else if (service === 'facebook') {
+        var width = 420,
+          height = 480,
+          left = document.documentElement.clientWidth/2 - width/2,
+          top = document.documentElement.clientHeight/2 - height/2;
+
+        return window.open(
+          '/noo/login/facebook',
+          'facebookAuth',
+          format('width=%s, height=%s, titlebar=no, toolbar=no, menubar=no, left=%s, top=%s', width, height, left, top)
+        );
+
       }
     }
   };
