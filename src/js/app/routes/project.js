02fe2271827f3cf2f872420ac990a257bcb8bc5e
Lawrence Wang <l@lw.io>
2015-05-08 18:06:07 -0700

    add draft bar at top of project page
    and stub out requests/contributors tabs

--- src/js/app/routes/project.js
+++ src/js/app/routes/project.js
@@ -16,21 +16,36 @@ module.exports = function ($stateProvider) {
       }
     }
   })
-  .state('project', /*@ngInject*/ {
-    url: '/project/:slug',
+  .state('editProject', {
+    url: '/project/:id/edit',
     parent: 'main',
     resolve: {
+      project: /*@ngInject*/ function(Project, $stateParams) {
+        return Project.get({id: $stateParams.id}).$promise;
+      }
+    },
+    views: {
+      main: {
+        templateUrl: '/ui/project/edit.tpl.html',
+        controller: 'ProjectEditCtrl'
+      }
+    }
+  })
+  .state('project', /*@ngInject*/ {
+    url: '/project/:id',
+    parent: 'main',
+    abstract: true,
+    resolve: {
       project: function(Project, $stateParams) {
-        return Project.get({slug: $stateParams.slug}).$promise;
+        return Project.get({id: $stateParams.id}).$promise;
       }
     },
     views: {
       main: {
         templateUrl: '/ui/project/show.tpl.html',
-        controller: function($scope, project) {
+        controller: function($scope, project, currentUser, growl) {
           $scope.project = project;
-          $scope.isCreator = true;
-          $scope.selectedTab = 'requests';
+          $scope.isCreator = currentUser && project.user_id === currentUser.id;
 
           $scope.details = truncate(project.details || '', 420);
           $scope.truncatedDetails = project.details && $scope.details !== project.details;
@@ -38,22 +53,37 @@ module.exports = function ($stateProvider) {
             $scope.details = project.details;
             $scope.truncatedDetails = false;
           };
+
+          $scope.publish = function() {
+            project.publish(function(resp) {
+              project.published_at = resp.published_at;
+              growl.addSuccessMessage('Published! You can unpublish from the Edit Project screen if you change your mind.');
+            });
+          };
+
         }
       }
     }
   })
-  .state('editProject', {
-    url: '/project/:slug/edit',
-    parent: 'main',
-    resolve: {
-      project: /*@ngInject*/ function(Project, $stateParams) {
-        return Project.get({slug: $stateParams.slug}).$promise;
-      }
-    },
+  .state('project.requests', /*@ngInject*/ {
+    url: '',
     views: {
-      main: {
-        templateUrl: '/ui/project/edit.tpl.html',
-        controller: 'ProjectEditCtrl'
+      tab: {
+        templateUrl: '/ui/project/requests.tpl.html',
+        controller: function($scope) {
+          $scope.foo = 'i am foo.';
+        }
+      }
+    }
+  })
+  .state('project.contributors', /*@ngInject*/ {
+    url: '/contributors',
+    views: {
+      tab: {
+        template: 'contributors tab placeholder. {{foo}}',
+        controller: function($scope) {
+          $scope.foo = 'controller stub is working.';
+        }
       }
     }
   });
--- src/js/app/services/Project.js
+++ src/js/app/services/Project.js
@@ -1,12 +1,23 @@
 var factory = function($resource) {
-  var Project = $resource('/noo/project/:slug', {
-    slug: '@slug'
+  var Project = $resource('/noo/project/:id', {
+    id: '@id'
   }, {
 
   });
 
   _.extend(Project.prototype, {
-    // TBD
+    update: function(params, success, error) {
+      return Project.save(_.extend({id: this.id}, params), success, error);
+    },
+    publish: function(success, error) {
+      return Project.save({id: this.id, publish: true}, success, error);
+    },
+    unpublish: function(success, error) {
+      return Project.save({id: this.id, unpublish: true}, success, error);
+    },
+    isPublished: function() {
+      return !!this.published_at;
+    }
   });
 
   return Project;
