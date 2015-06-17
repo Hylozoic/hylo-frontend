9886a2c388af2b904399e084340c2c0f37446b48
Lawrence Wang <l@lw.io>
2015-06-16 22:13:13 -0700

    fix handling of "status 0" errors
    they indicate request cancellation or general network issues

--- src/js/app/services/myHttpInterceptor.js
+++ src/js/app/services/myHttpInterceptor.js
@@ -2,10 +2,15 @@ var config = function ($httpProvider) {
   $httpProvider.interceptors.push(function ($q, growl) {
     return {
       responseError: function(rejection) {
-        if (!_.include([401, 403, 422], rejection.status)) {
+        if (!_.include([401, 403, 422, 0], rejection.status)) {
           Rollbar.error(format('%s: %s', rejection.status, rejection.config.url));
           var message = format('Oops! An error occurred. The Hylo team has been notified. (%s)', rejection.status);
           growl.addErrorMessage(message, {ttl: 5000});
+        }
+
+        if (rejection.status === 0) {
+          Rollbar.error('Timeout', {url: rejection.config.url});
+          growl.addErrorMessage("Oops! Your request timed out. Please try again.", {ttl: 5000});
         }
 
         return $q.reject(rejection);
