70b1db8e04ee3d9ed1f0a695e63b31ae9f0acc80
Lawrence Wang <l@lw.io>
2015-09-11 10:24:03 -0700

    Post.relevantCommunity shouldn't assume user is present

--- src/js/app/services/Post.js
+++ src/js/app/services/Post.js
@@ -60,6 +60,7 @@ var factory = function ($resource) {
   })
 
   Post.relevantCommunity = function (post, user) {
+    if (!user) return null
     var ids = _.pluck(user.memberships, 'community_id')
     return _.find(post.communities, c => _.contains(ids, c.id))
   }
