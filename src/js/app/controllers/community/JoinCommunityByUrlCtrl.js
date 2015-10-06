fa93abd84895829b1671664745f310eaf1a68fb5
Issac Trotts <issac.trotts@gmail.com>
2015-10-05 23:35:34 -0700

    Join community button now joins community.
    This breaks the Join Community menu item.

--- src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
+++ src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
@@ -1,12 +1,5 @@
-var controller = function($scope, $analytics, community, currentUser) {
+var controller = function($scope, community) {
   $scope.community = community;
-  $scope.canModerate = currentUser && currentUser.canModerate(community);
-
-  $analytics.eventTrack('Community: Join Community By URL', {
-    community_id: community.id,
-    community_name: community.name,
-    community_slug: community.slug
-  });
 };
 
 module.exports = function(angularModule) {
