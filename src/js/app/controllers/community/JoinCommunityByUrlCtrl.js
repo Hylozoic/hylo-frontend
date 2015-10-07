fec2b9bc9a98656ecc9f2aeb4cb80b6df3aced85
Issac Trotts <issac.trotts@gmail.com>
2015-10-06 18:13:02 -0700

    Show login/signup dialog if not logged in.

--- src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
+++ src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
@@ -1,10 +1,57 @@
-module.exports = function($scope, $controller, community, code) {
+module.exports = function($scope, $controller, community, code, currentUser, $modal) {
   'ngInject';
 
   $scope.community = community;
   $scope.code = code;
 
+  if (!currentUser) {
+
+    var defaults = {
+      backdrop: true,
+      keyboard: false,
+      windowClass: 'login-signup-modal',
+      resolve: {
+        context: function() { return 'modal' },
+        projectInvitation: null
+      }
+    };
+
+    var handle = function(result) {
+      if (result.action === 'go') {
+        open(result.state);
+      } else if (result.action === 'finish') {
+        $scope.$state.reload();
+      }
+    };
+
+    var open = function(state) {
+      var options = {};
+      if (state === 'signup') {
+        _.merge(options, {
+          templateUrl: '/ui/entrance/signup.tpl.html',
+          controller: 'SignupCtrl',
+        }, defaults);
+      } else if (state === 'login') {
+        _.merge(options, {
+          templateUrl: '/ui/entrance/login.tpl.html',
+          controller: 'LoginCtrl',
+        }, defaults);
+      } else if (state === 'forgotPassword') {
+        _.merge(options, {
+          templateUrl: '/ui/entrance/forgot-password.tpl.html',
+          controller: 'ForgotPasswordCtrl',
+        }, defaults);
+      }
+      $modal.open(options).result.then(handle);
+    };
+
+    open('signup');
+
+  }
+
+  // Put functions from JoinCommunityCtrl onto $scope.
   $controller('JoinCommunityCtrl', {$scope: $scope})
+
   // Validate the community join code from last part of the URL.
   $scope.validateCode();
 };
