b383cdf34c567214d69c00e9e9a1cf664ccb04b3
Robbie Carlton <robbie.carlton@gmail.com>
2015-10-22 15:45:54 -0700

    logic for event group headers

--- src/js/app/controllers/community/CommunityEventsCtrl.js
+++ src/js/app/controllers/community/CommunityEventsCtrl.js
@@ -1,3 +1,5 @@
+var moment = require('moment-timezone')
+
 module.exports = function ($scope, community, currentUser, firstPostQuery, Post, PostManager) {
   'ngInject'
 
@@ -23,5 +25,54 @@ module.exports = function ($scope, community, currentUser, firstPostQuery, Post,
 
   $scope.updateView = function () {
     postManager.reload()
+  }
+
+  $scope.eventHeader = function (previous, current) {
+    var timeCategory = function (event) {
+      var now = moment()
+      var eventTime = moment(event.start_time)
+      var difference = eventTime.diff(now, 'days')
+      if (difference < 0) {
+        return 'past'
+      } else if (difference === 0) {
+        return 'today'
+      } else if (difference === 1) {
+        return 'tomorrow'
+      } else if (difference < 8 && eventTime.week() === now.week()) {
+        return 'week'
+      } else if (difference < 31 && eventTime.month() === now.month()) {
+        return 'month'
+      } else {
+        return 'future'
+      }
+    }
+
+    var headerFromCategory = function (category) {
+      switch (category) {
+        case 'past':
+          return 'Past Events'
+        case 'today':
+          return 'Todays Events'
+        case 'tomorrow':
+          return 'Tomorrows Events'
+        case 'week':
+          return 'This Weeks Events'
+        case 'month':
+          return 'This Months Events'
+        case 'future':
+          return 'Future Events'
+        default:
+          return null
+      }
+    }
+
+    var currentCategory = timeCategory(current)
+
+    if (previous) {
+      var previousCategory = timeCategory(previous)
+      if (previousCategory === currentCategory) return
+    }
+
+    return headerFromCategory(currentCategory)
   }
 }
