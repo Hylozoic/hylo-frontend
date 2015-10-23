c9cd491864311c5e02cc12476c5ca68a631111e4
Robbie Carlton <robbie.carlton@gmail.com>
2015-10-23 11:25:17 -0700

    unnest group function

--- src/js/app/controllers/community/CommunityEventsCtrl.js
+++ src/js/app/controllers/community/CommunityEventsCtrl.js
@@ -27,28 +27,28 @@ module.exports = function ($scope, community, currentUser, firstPostQuery, Post,
     postManager.reload()
   }
 
-  $scope.eventHeader = function (previous, current) {
-    var group = function (event) {
-      var now = moment()
-      var eventTime = moment(event.start_time)
-      var difference = eventTime.diff(now, 'days')
-      if (difference < 0) {
-        return 'Past'
-      } else if (difference === 0) {
-        return 'Today'
-      } else if (difference === 1) {
-        return 'Tomorrow'
-      } else if (difference < 8 && eventTime.isoWeek() === now.isoWeek()) {
-        return 'This Week'
-      } else if (difference < 15 && eventTime.isoWeek() === now.isoWeek() + 1) {
-        return 'Next Week'
-      } else if (difference < 31 && eventTime.month() === now.month()) {
-        return 'Month'
-      } else {
-        return 'Future'
-      }
+  var group = function (event) {
+    var now = moment()
+    var eventTime = moment(event.start_time)
+    var difference = eventTime.diff(now, 'days')
+    if (difference < 0) {
+      return 'Past'
+    } else if (difference === 0) {
+      return 'Today'
+    } else if (difference === 1) {
+      return 'Tomorrow'
+    } else if (difference < 8 && eventTime.isoWeek() === now.isoWeek()) {
+      return 'This Week'
+    } else if (difference < 15 && eventTime.isoWeek() === now.isoWeek() + 1) {
+      return 'Next Week'
+    } else if (difference < 31 && eventTime.month() === now.month()) {
+      return 'Month'
+    } else {
+      return 'Future'
     }
+  }
 
+  $scope.eventHeader = function (previous, current) {
     var currentGroup = group(current)
 
     if (previous) {
