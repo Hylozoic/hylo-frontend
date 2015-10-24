a14120197e4d53cd6c52f6fe0a1d23bd2217b085
Lawrence Wang <l@lw.io>
2015-10-23 17:44:14 -0700

    cleanup

--- src/js/app/services/UserMentions.js
+++ src/js/app/services/UserMentions.js
@@ -1,10 +1,10 @@
-module.exports = function(Community, User, $rootScope) {
-  'ngInject';
+module.exports = function (Community, User, $rootScope) {
+  'ngInject'
   return {
-    userTextRaw: function(user) {
+    userTextRaw: function (user) {
       return format(
         '<a contenteditable="false" tabindex="-1" target="_blank" href="/u/%s" data-user-id="%s">@%s</a>',
-        user.id, user.id, user.name);
+        user.id, user.id, user.name)
     }
-  };
-};
+  }
+}
