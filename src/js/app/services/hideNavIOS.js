febaf5a9d994ccaec54bbe460a0b657dd9d86664
Robbie Carlton <robbie.carlton@gmail.com>
2015-12-01 12:56:06 -0800

    remove log

--- src/js/app/services/hideNavIOS.js
+++ src/js/app/services/hideNavIOS.js
@@ -3,10 +3,8 @@ var isIOSApp = require('./isIOSApp')
 module.exports = function (hide) {
   if (isIOSApp()) {
     if (hide) {
-      console.log('hideNavIOS(true)')
       document.getElementById('nav').style.display = 'none'
     } else {
-      console.log('hideNavIOS(false)')
       document.getElementById('nav').style.display = 'block'
     }
   }
