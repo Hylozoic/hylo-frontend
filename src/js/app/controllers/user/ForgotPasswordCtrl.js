ff2f0d4e8421f9accb89eec1250337176ef28830
Lawrence Wang <l@lw.io>
2016-03-01 12:14:03 -0800

    defend against multiple password reset form submissions

--- src/js/app/controllers/user/ForgotPasswordCtrl.js
+++ src/js/app/controllers/user/ForgotPasswordCtrl.js
@@ -1,32 +1,35 @@
-var controller = function($scope, $analytics, User, context) {
-  $analytics.eventTrack('Password reset start');
+var controller = function ($scope, $analytics, User, context) {
+  $analytics.eventTrack('Password reset start')
+  var requestPending = false
 
-  $scope.submit = function(form) {
-    form.submitted = true;
-    $scope.error = null;
-    if (form.$invalid) return;
+  $scope.submit = function (form) {
+    form.submitted = true
+    $scope.error = null
+    if (form.$invalid || requestPending) return
 
-    User.requestPasswordChange({email: $scope.email}, function(resp) {
+    requestPending = true
+
+    User.requestPasswordChange({email: $scope.email}, function (resp) {
       if (!resp.error) {
-        $analytics.eventTrack('Password reset success', {email: $scope.email});
-        $scope.success = "Please click the link in the email that we just sent you.";
+        $analytics.eventTrack('Password reset success', {email: $scope.email})
+        $scope.success = 'Please click the link in the email that we just sent you.'
       } else if (resp.error === 'no user') {
-        $analytics.eventTrack('Password reset failure', {email: $scope.email});
-        $scope.error = 'The email address you entered was not recognized.';
+        $analytics.eventTrack('Password reset failure', {email: $scope.email})
+        $scope.error = 'The email address you entered was not recognized.'
       }
-    });
-  };
+      requestPending = false
+    })
+  }
 
-  $scope.go = function(state) {
+  $scope.go = function (state) {
     if (context === 'modal') {
-      $scope.$close({action: 'go', state: state});
+      $scope.$close({action: 'go', state: state})
     } else {
-      $scope.$state.go(state);
+      $scope.$state.go(state)
     }
-  };
+  }
+}
 
-};
-
-module.exports = function(angularModule) {
-  angularModule.controller('ForgotPasswordCtrl', controller);
-};
+module.exports = function (angularModule) {
+  angularModule.controller('ForgotPasswordCtrl', controller)
+}
