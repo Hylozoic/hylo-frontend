b39c27759f63f76192766bc1cb71aa07c6ed8b6d
Lawrence Wang <l@lw.io>
2016-03-08 12:34:19 -0800

    revise join link behavior when already joined
    just redirect to community

--- src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
+++ src/js/app/controllers/community/JoinCommunityByUrlCtrl.js
@@ -1,14 +1,21 @@
-module.exports = function($scope, $controller, community, code, currentUser, $modal) {
-  'ngInject';
+module.exports = function ($scope, $controller, community, code, currentUser,
+  $modal, growl, $timeout) {
+  'ngInject'
 
-  $scope.community = community;
-  $scope.code = code;
+  $scope.community = community
+  $scope.code = code
 
-  $scope.alreadyJoined = !!_.find(currentUser.memberships, m => m.community_id === community.id);
+  var membership = _.find(currentUser.memberships, m => m.community_id === community.id)
+  if (membership) {
+    $timeout(() => {
+      $scope.$state.go('community.posts', {community: membership.community.slug})
+      growl.addSuccessMessage('You are already a member of this community.')
+    })
+  } else {
+    // Put functions from JoinCommunityCtrl onto $scope.
+    $controller('JoinCommunityCtrl', {$scope: $scope})
 
-  // Put functions from JoinCommunityCtrl onto $scope.
-  $controller('JoinCommunityCtrl', {$scope: $scope})
-
-  // Validate the community join code from last part of the URL.
-  $scope.validateCode();
+    // Validate the community join code from last part of the URL.
+    $scope.validateCode()
+  }
 }
